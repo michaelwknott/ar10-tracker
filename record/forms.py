13cb043dc2f419eec37004cbeeb8684026302617
Michael Knott <michaelknott@live.co.uk>
2023-02-09 16:10:17 +0000

    Create forms for CompetitionData and TrainingData

--- record/forms.py
+++ record/forms.py
@@ -1,7 +1,7 @@
-from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.forms import ModelForm
 
-from .models import CustomUser
+from .models import CompetitionData, CustomUser, TrainingData
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -14,3 +14,15 @@ class CutomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("email",)
+
+
+class TrainingDataForm(ModelForm):
+    class Meta:
+        model = TrainingData
+        fields = "__all__"
+
+
+class CompetitionDataForm(ModelForm):
+    class Meta:
+        model = CompetitionData
+        fields = "__all__"
