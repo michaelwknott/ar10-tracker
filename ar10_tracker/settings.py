2e1a5ee5021329245bd753d4f6994a2af3068d6f
Michael Knott <michaelknott@live.co.uk>
2023-02-10 12:10:42 +0000

    Create base template

--- ar10_tracker/settings.py
+++ ar10_tracker/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "ar10_tracker.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
