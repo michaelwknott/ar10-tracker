8f9e3eca9a52b4854f685b982d6ffcbe620df531
Michael Knott <michaelknott@live.co.uk>
2023-03-28 22:02:12 +0100

    Update dashboard view and template

--- record/views.py
+++ record/views.py
@@ -12,36 +12,25 @@ def dashboard(request):
     # Competition data
     competition_data = CompetitionData.objects.all()
 
-    # get personal best score from competition data
-    comp_pb = CompetitionData.objects.aggregate(Max("qual_score"))
+    # Get personal best score from competition data
+    comp_pb = competition_data.aggregate(Max("qual_score"))
 
-    # get average of top 3 scores from competition data
-    comp_top3 = CompetitionData.objects.order_by("qual_score")[:3]
-    comp_top3_avg = comp_top3.aggregate(Avg("qual_score"))
-
-    # get individual dates and scores from competition data queryset
-    comp_dates = []
-    qual_scores = []
-
-    for competition in competition_data:
-        comp_date = competition.date.strftime("%Y.%m.%d")
-        qual_score = competition.qual_score
-
-        comp_dates.append(comp_date)
-        qual_scores.append(qual_score)
+    # Get average of top 3 scores from competition data
+    comp_top3_avg = competition_data.order_by("-qual_score")[:3].aggregate(
+        Avg("qual_score")
+    )
 
     # Training data
     training_data = TrainingData.objects.all()
 
-    # get personal best score from training data
-    training_pb = TrainingData.objects.aggregate(Max("score"))
+    # Get personal best score from training data
+    training_pb = training_data.aggregate(Max("score"))
 
-    # get average of top 3 scores from training data
-    training_top3 = TrainingData.objects.order_by("score")[:3]
-    training_top3_avg = training_top3.aggregate(Avg("score"))
+    # Get average of top 3 scores from training data
+    training_top3_avg = training_data.order_by("-score")[:3].aggregate(Avg("score"))
 
-    # get average of all training data fileds
-    training_data_avgs = TrainingData.objects.aggregate(
+    # Get average of all training data fields
+    training_data_avgs = training_data.aggregate(
         Avg("ten_zero"),
         Avg("ten_azero"),
         Avg("ten_five"),
@@ -52,43 +41,14 @@ def dashboard(request):
         Avg("da"),
     )
 
-    # get individual metrics from training data queryset
-    training_dates = []
-    training_scores = []
-    ten_zeros = []
-    ten_azeros = []
-    ten_fives = []
-    ten_afives = []
-
-    for training in training_data:
-        training_date = training.date.strftime("%Y.%m.%d")
-        training_score = training.score
-        ten_five = training.ten_five
-        ten_afive = training.ten_afive
-        ten_zero = training.ten_zero
-        ten_azero = training.ten_azero
-
-        training_dates.append(training_date)
-        training_scores.append(training_score)
-        ten_zeros.append(ten_zero)
-        ten_azeros.append(ten_azero)
-        ten_fives.append(ten_five)
-        ten_afives.append(ten_afive)
-
     context = {
-        "training_data_avgs": training_data_avgs,
-        "comp_dates": comp_dates,
-        "qual_scores": qual_scores,
-        "training_dates": training_dates,
-        "training_scores": training_scores,
-        "ten_azeros": ten_azeros,
-        "ten_zeros": ten_zeros,
-        "ten_afives": ten_afives,
-        "ten_fives": ten_fives,
+        "competition_data": competition_data,
+        "training_data": training_data,
         "comp_top3_avg": comp_top3_avg,
         "comp_pb": comp_pb,
         "training_top3_avg": training_top3_avg,
         "training_pb": training_pb,
+        "training_data_avgs": training_data_avgs,
     }
     return render(request, "dashboard.html", context=context)
 
