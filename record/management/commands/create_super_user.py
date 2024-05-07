590772992e6f6d12b93f5a2b864166494703742b
Michael Knott <michaelknott@live.co.uk>
2024-05-07 11:28:49 +0100

    fix: use correct fields for CustomUser

--- record/management/commands/create_super_user.py
+++ record/management/commands/create_super_user.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 
 from record.models import CustomUser
 
-USERNAME = config("ADMIN_USERNAME")
+EMAIL = config("ADMIN_EMAIL")
 PASSWORD = config("ADMIN_PASSWORD")
 
 
@@ -11,10 +11,9 @@ class Command(BaseCommand):
     help = "Creates a superuser."
 
     def handle(self, *args, **options):
-        if not CustomUser.objects.filter(username=USERNAME).exists():
+        if not CustomUser.objects.filter(email=EMAIL).exists():
             CustomUser.objects.create_superuser(
-                username=USERNAME,
-                email=USERNAME,
+                email=EMAIL,
                 password=PASSWORD,
             )
         print("Superuser has been created.")
